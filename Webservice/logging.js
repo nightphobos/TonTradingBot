fda1d7bc7818c2121d8309a5ba24f688d60d3f80
dusanpraca-Server <davepracadev@hotmail.com>
2024-03-30 07:16:10 +0100

    deleted:    Webservice/logging.js 	deleted:    Webservice/package-lock.json 	deleted:    Webservice/package.json 	deleted:    Webservice/proxy.js 	deleted:    Webservice/routes.js 	deleted:    Webservice/server.js

--- Webservice/logging.js
+++
@@ -1,7 +0,0 @@
-const morgan = require("morgan");
-
-const setupLogging = (app) => {
-    app.use(morgan('combined'));
-}
-
-exports.setupLogging = setupLogging
--- Webservice/proxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-const setupProxies = (app, routes) => {
-    routes.forEach(r => {
-        app.use(r.url, createProxyMiddleware(r.proxy));
-    })
-}
-
-exports.setupProxies = setupProxies
